d4dff3be9c99d2cf77a81ccfc5a43e4a4224daca
Tom <tomjuggler@gmail.com>
2020-10-22 22:14:35 +0200

    a lot of testing of Tabulator, setup and functionality array from Table Headers
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ APP_TITLE = "Ontology Spreadsheet Editor"
 
 DATABASE_URI = 'sqlite:////tmp/github-flask-ontospreaded.db'
 
-GITHUB_CLIENT_ID = 'EDIT_ME'
-GITHUB_CLIENT_SECRET = 'EDIT_ME'
+GITHUB_CLIENT_ID = 'd4bfb2374ed983a6fe7d'
+GITHUB_CLIENT_SECRET = '4ad7848e04210f22fe1db818bbb15266046f9250'
 
 REPOSITORIES = {"AddictO": "jannahastings/addiction-ontology"}
