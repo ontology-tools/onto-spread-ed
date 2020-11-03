f68ba5b6081a0994eee628231fa86478b58b5a48
Janna Hastings <janna.hastings@babraham.ac.uk>
2020-11-03 22:03:06 +0100

    Add skeleton of save function

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 import json
 
 from flask import Flask, request, g, session, redirect, url_for, render_template
-from flask import render_template_string, jsonify
+from flask import render_template_string, jsonify, Response
 from flask_github import GitHub
 
 from sqlalchemy import create_engine, Column, Integer, String
@@ -204,6 +204,87 @@ def edit(repo_key, folder, spreadsheet):
                             header=json.dumps(header),
                             rows=json.dumps(rows)
                             )
+
+
+@app.route('/save', methods=['POST'])
+@verify_logged_in
+def save():
+    repo_key = request.form.get("repo_key")
+    folder = request.form.get("folder")
+    spreadsheet = request.form.get("spreadsheet")
+    row_data = request.form.get("rowData")
+    commit_msg = request.form.get("commit_msg")
+
+    repositories = app.config['REPOSITORIES']
+    repo_detail = repositories[repo_key]
+
+    # (Re) load the existing spreadsheet
+    #spreadsheet_file = github.get(
+    #    f'repos/{repo_detail}/contents/{folder}/{spreadsheet}'
+    #)
+    #base64_bytes = spreadsheet_file['content'].encode('utf-8')
+    #decoded_data = base64.decodebytes(base64_bytes)
+    #wb = openpyxl.load_workbook(io.BytesIO(decoded_data))
+
+
+    # TODO Check if you CAN edit this version of the spreadsheet, i.e. that it
+    # TODO didn't have any concurrent changes that can't be merged.
+
+
+    # TODO Update the values in the spreadsheet with the new values
+
+    # Create version for saving
+    #spreadsheet_stream = io.BytesIO()
+    #wb.save(spreadsheet_stream)
+    #encoded_data = base64.encode(spreadsheet_stream.getvalue())
+
+    # Create a new branch to commit the change to
+    #response = github.get(f"repos/{repo_detail}/git/ref/heads/main")
+    #if not response or "object" not in response or "sha" not in response["object"]:
+    #    raise Exception(f"Unable to get SHA for HEAD of main in {repo_detail}")
+    #sha = response["object"]["sha"]
+    #branch = f"{g.user.github_login}_{datetime.utcnow().strftime('%Y-%m-%d_%H%M')}"
+    #response = github.post(
+    #    f"repos/{repo_detail}/git/refs", data={"ref": f"refs/heads/{branch}", "sha": sha},
+    #    )
+    #if not response:
+    #    raise Exception(f"Unable to create new branch {branch} in {repo_detail}")
+
+    # Get the sha for the file
+    #response = github.get(f"repos/{repo_detail}/contents/{folder}/{spreadsheet}")
+    #if not response or "sha" not in response:
+    #    raise Exception(
+    #        f"Unable to get the current SHA value for {spreadsheet} in {repo_detail}/{folder}"
+    #        )
+
+    # Commit changes to branch (replace code with sheet)
+    #data = {
+    #    "message": commit_msg,
+    #    "content": base64.b64encode(code.encode("utf-8")).decode(),
+    #    "branch": branch,
+    #}
+
+    #if file_sha:
+    #    data["sha"] = file_sha
+
+    #response = github.put(f"repos/{repo_detail}/contents/{folder}/{spreadsheet}", data=data)
+    #if not response:
+    #    raise Exception(
+    #        f"Unable to commit addition of {spreadsheet} to branch {branch} in {repo_detail}"
+    #    )
+
+    #response = github.post(
+    #    f"repos/{repo}/pulls",
+    #    data={
+    #        "title": commit_msg,
+    #        "head": branch,
+    #        "base": "main",
+    #    },
+    #)
+    #if not response:
+    #    raise Exception(f"Unable to create PR for branch {branch} in {repo_detail}")
+
+    return Response(status=200) # (Pretend) Success
 
 
 if __name__ == "__main__":        # on running python app.py
