3264cbf018ef8613cac366a9222262c445728f4c
Janna Hastings <janna.hastings@babraham.ac.uk>
2020-11-17 16:15:25 +0100

    Better config import handling

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # setup sqlalchemy
 
-exec(open('config.py').read())
+from config import *
 
 engine = create_engine(DATABASE_URI)
 db_session = scoped_session(sessionmaker(autocommit=False,
