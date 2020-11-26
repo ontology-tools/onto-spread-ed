43a6103795a5ab327cd3347a8527280131a55e6c
Janna Hastings <janna.hastings@babraham.ac.uk>
2020-11-26 17:01:12 +0100

    Fix logged out / already logged in bug

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def verify_logged_in(fn):
     def wrapped(*args, **kwargs):
         # If the user is not logged in, then redirect him to the "logged out" page:
         if not g.user:
-            return redirect(url_for("loggedout"))
+            return redirect(url_for("logout"))
         return fn(*args, **kwargs)
 
     return wrapped
