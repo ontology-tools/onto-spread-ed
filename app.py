b4ef056ce668c152557b31f03724854f31c1ddf1
Tom <tomjuggler@gmail.com>
2021-08-24 21:25:07 +0200

    Added descendants to graph in "Visualise sheet"
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- app.py
+++ app.py
@@ -420,23 +420,23 @@ class OntologyDataStore:
                                 if entryParent in self.label_to_id:
                                     ids.append(self.label_to_id[entryParent])
                             #below was missing before... do we need to include this?
-                            # entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
-                            # if entryIri:
-                            #     descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
-                            #     for d in descs:
-                            #         ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
-                            # if self.graphs[repo]:
-                            #     graph_descs = None
-                            #     try:
-                            #         graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
-                            #     except networkx.exception.NetworkXError:
-                            #         print("NetworkXError: ", entry['ID'])
-                            #         # print("networkx exception error in getIDsFromSelection", id)
+                            entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
+                            if entryIri:
+                                descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
+                                for d in descs:
+                                    ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
+                            if self.graphs[repo]:
+                                graph_descs = None
+                                try:
+                                    graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
+                                except networkx.exception.NetworkXError:
+                                    print("NetworkXError: ", entry['ID'])
+                                    # print("networkx exception error in getIDsFromSelection", id)
                                 
-                            #     if graph_descs is not None:
-                            #         for g in graph_descs:
-                            #             if g not in ids:
-                            #                 ids.append(g)
+                                if graph_descs is not None:
+                                    for g in graph_descs:
+                                        if g not in ids:
+                                            ids.append(g)
                             
                             
         return (ids)
@@ -461,23 +461,23 @@ class OntologyDataStore:
                             if entryParent in self.label_to_id:
                                 ids.append(self.label_to_id[entryParent])
                         #todo: below was missing before... do we need to include this?
-                        # entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
-                        # if entryIri:
-                        #     descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
-                        #     for d in descs:
-                        #         ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
-                        # if self.graphs[repo]:
-                        #     graph_descs = None
-                        #     try:
-                        #         graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
-                        #     except networkx.exception.NetworkXError:
-                        #         print("NetworkXError: ", entry['ID'])
-                        #         # print("networkx exception error in getIDsFromSelection", id)
+                        entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
+                        if entryIri:
+                            descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
+                            for d in descs:
+                                ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
+                        if self.graphs[repo]:
+                            graph_descs = None
+                            try:
+                                graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
+                            except networkx.exception.NetworkXError:
+                                print("NetworkXError: ", entry['ID'])
+                                # print("networkx exception error in getIDsFromSelection", id)
                             
-                        #     if graph_descs is not None:
-                        #         for g in graph_descs:
-                        #             if g not in ids:
-                        #                 ids.append(g)  
+                            if graph_descs is not None:
+                                for g in graph_descs:
+                                    if g not in ids:
+                                        ids.append(g)  
                 else:
                     if 'ID' in entry and len(entry['ID'])>0:
                             ids.append(entry['ID'].replace(":","_"))
@@ -487,23 +487,23 @@ class OntologyDataStore:
                         if entryParent in self.label_to_id:
                             ids.append(self.label_to_id[entryParent])
                     #todo: below was missing before... do we need to include this? 
-                    # entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
-                    # if entryIri:
-                    #     descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
-                    #     for d in descs:
-                    #         ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
-                    # if self.graphs[repo]:
-                    #     graph_descs = None
-                    #     try:
-                    #         graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
-                    #     except networkx.exception.NetworkXError:
-                    #         print("NetworkXError: ", entry['ID'])
-                    #         # print("networkx exception error in getIDsFromSelection", id)
+                    entryIri = self.releases[repo].get_iri_for_id(entry['ID'])                    
+                    if entryIri:
+                        descs = pyhornedowl.get_descendants(self.releases[repo], entryIri)
+                        for d in descs:
+                            ids.append(self.releases[repo].get_id_for_iri(d).replace(":", "_"))
+                    if self.graphs[repo]:
+                        graph_descs = None
+                        try:
+                            graph_descs = networkx.algorithms.dag.descendants(self.graphs[repo],entry['ID'].replace(":", "_"))
+                        except networkx.exception.NetworkXError:
+                            print("NetworkXError: ", entry['ID'])
+                            # print("networkx exception error in getIDsFromSelection", id)
                         
-                    #     if graph_descs is not None:
-                    #         for g in graph_descs:
-                    #             if g not in ids:
-                    #                 ids.append(g)   
+                        if graph_descs is not None:
+                            for g in graph_descs:
+                                if g not in ids:
+                                    ids.append(g)   
         return (ids)
     
     def getIDsFromSelectionMultiSelect(self, repo, data, selectedIds, filter):
