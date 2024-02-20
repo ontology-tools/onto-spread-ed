a2a6c37b04bf3e86ee7c30b8a6dc34784209f4bd
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2024-02-20 08:04:57 +0100

    Added utility function

--- onto_spread_ed/utils/__init__.py
+++ onto_spread_ed/utils/__init__.py
@@ -5,3 +5,7 @@ from .github import *  # noqa: F403, F401
 
 def str_empty(value: typing.Optional[str]) -> bool:
     return value is None or not any(value.strip())
+
+
+def lower(string: Optional[str]) -> Optional[str]:
+    return string.lower() if string is not None else None
