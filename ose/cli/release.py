6ee7ad24fddc3a703418c443d68f99e7a3a32f8c
Björn <5106696+b-gehrke@users.noreply.github.com>
2025-02-24 13:25:11 +0100

    Removed unimplemented code

--- ose/cli/release.py
+++ ose/cli/release.py
@@ -12,14 +12,3 @@ def init_commands(cli: AppGroup, inject: Callable[[Any], Callable[[tuple[Any, ..
     @cli.group("release")
     def group():
         pass
-
-    @group.command("build-external")
-    @click.option("--release_script", "-r", default=".onto-ed/release_script.json")
-    @click.option("--local_path", "-l", default=".")
-    @inject
-    def build(release_script: str, local_path: str, db: SQLAlchemy):
-        print(f"Building release script {release_script} at {local_path}")
-
-        step = BuildReleaseStep(db, None, )
-
-        pass
