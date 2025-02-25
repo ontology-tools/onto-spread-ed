f47b344d8b94139ef7f5227c78ff636ebc7d2229
Björn <5106696+b-gehrke@users.noreply.github.com>
2025-02-25 11:02:19 +0100

    Python compatability

--- ose/commands/CLICommandContext.py
+++ ose/commands/CLICommandContext.py
@@ -11,7 +11,7 @@ class CLICommandContext(CommandContext):
     _logger = logging.getLogger(__name__)
 
     _working_dir: str
-    _tempdir: Optional[TemporaryDirectory[str]] = None
+    _tempdir: Optional[TemporaryDirectory] = None
 
     def __init__(self, working_dir: Optional[str] = None):
         if working_dir is None:
@@ -26,7 +26,6 @@ class CLICommandContext(CommandContext):
     def local_name(self, remote_name, file_ending=None) -> str:
         file_name = os.path.join(self._working_dir, remote_name)
 
-
         if file_ending is not None:
             file_name = file_name[:file_name.rfind(".")] + file_ending
 
@@ -36,7 +35,6 @@ class CLICommandContext(CommandContext):
 
     def save_file(self, file: str, temporary: Optional[bool] = None, **kwargs):
         target_path = os.path.abspath(os.path.join(self._working_dir, file))
-
 
         if os.path.abspath(target_path) != os.path.abspath(file):
             self._logger.debug(f"Saving file '{file}' to '{target_path}'")
@@ -49,4 +47,3 @@ class CLICommandContext(CommandContext):
 
         if self._tempdir is not None:
             self._tempdir.cleanup()
-
